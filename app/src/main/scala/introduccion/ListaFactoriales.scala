e58f949b52f3e4b39f9612e1c796b48439acf648
Carlos Delgado <carlos.andres.delgado@correounivalle.edu.co>
2023-08-21 20:43:16 -0500

    test

--- app/src/main/scala/introduccion/ListaFactoriales.scala
+++ app/src/main/scala/introduccion/ListaFactoriales.scala
@@ -5,7 +5,7 @@ class ListaFactoriales {
     def objListaFactoriales(n : Int): List[BigInt] = {
         var listaFactoriales : List[BigInt] = List()
         val objFactorial: Factorial = new Factorial()
-        //Complete el código
+        //Complete el código por ahora pendiente
         throw new UnsupportedOperationException("No implementado aún")
     }
 }
